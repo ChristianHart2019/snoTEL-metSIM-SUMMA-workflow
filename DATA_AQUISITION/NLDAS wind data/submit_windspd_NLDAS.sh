6eab8c6da86258eaa5c438ac7580b0730b9fc6d7
ChristianHart2019 <56000254+ChristianHart2019@users.noreply.github.com>
2020-02-14 12:25:54 -0700

    Update submit_windspd_NLDAS.sh

--- DATA_AQUISITION/NLDAS wind data/submit_windspd_NLDAS.sh
+++ DATA_AQUISITION/NLDAS wind data/submit_windspd_NLDAS.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-#SBATCH --nodes=1
+#SBATCH --nodes=1               
 #SBATCH --time=0-01:00
 
+#load require modules
 module load python/3.7.4
 module load scipy-stack/2019a
 module load netcdf
 module laod hdf5
 
+# run the win acquistion script
 python3 windspd_NLDAS3.sh
     
