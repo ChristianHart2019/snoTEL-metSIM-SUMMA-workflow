8e3e60b92351ac009d4c2885633557859541bce7
ChristianHart2019 <56000254+ChristianHart2019@users.noreply.github.com>
2020-02-14 11:08:20 -0700

    Update INSTALL_MetSim.sh

--- METSIM_INSTALLATION/INSTALL_MetSim.sh
+++ METSIM_INSTALLATION/INSTALL_MetSim.sh
@@ -6,12 +6,17 @@ module load python/3.7.4
 module load scipy-stack/2019a
 module load netcdf
 module laod hdf5
-DOWNLOAD: curl https://bootstrap.pypa.io/get-pip.py -o get-pip.y
+
+#DOWNLOAD: pip module 
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.y
+
 pip install scipy --user
 pip install netcdf4 --user
 pip install numba --user
 pip install pandas --user
 pip install xarray --user
+
 DOWNLOAD: MetSim form Github
+
 cd MetSim
 python3 setup.py install --user
