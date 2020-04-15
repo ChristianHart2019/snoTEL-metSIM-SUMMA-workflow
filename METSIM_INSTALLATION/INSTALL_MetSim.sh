0a1f6ad567e4c74c8e8f5b904ce14dc76db1f067
ChristianHart2019 <56000254+ChristianHart2019@users.noreply.github.com>
2020-04-15 10:35:36 -0600

    Update INSTALL_MetSim.sh

--- METSIM_INSTALLATION/INSTALL_MetSim.sh
+++ METSIM_INSTALLATION/INSTALL_MetSim.sh
@@ -16,7 +16,7 @@ pip install numba --user
 pip install pandas --user
 pip install xarray --user
 
-DOWNLOAD: MetSim form Github
+DOWNLOAD: MetSim from Github
 
 cd MetSim
 python3 setup.py install --user
